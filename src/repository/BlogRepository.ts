3f19b12f85bd1d776db3df6184ac466d61a8a15b
wookkl <wjddnr315@gmail.com>
2022-05-05 23:05:00 +0900

    Add blog repository

---
+++ src/repository/BlogRepository.ts
@@ -0,0 +1,21 @@
+import {EntityRepository, Repository} from "typeorm";
+import {Blog} from "../entity/Blog";
+import {DeepPartial} from "typeorm/common/DeepPartial";
+
+@EntityRepository(Blog)
+export class BlogRepository extends Repository<Blog> {
+  async getOrCreate(entityLike: DeepPartial<Blog>, defaultData?: DeepPartial<Blog>) {
+    let blog = await super.findOne({
+      relations: ["user"],
+      where: entityLike,
+    });
+    if (blog === undefined) {
+      const createData = {
+        ...entityLike,
+        ...defaultData
+      }
+      blog = await super.create(createData);
+      await super.save(blog);
+    }
+  }
+}
